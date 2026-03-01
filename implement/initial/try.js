ead74b2eb0dacb0d1c5a3de567c8ffef138bcf00
Storm-shadow-beep <ehudjf9@gmail.com>
2026-03-01 21:41:32 +0300

    fix the mode button issue

--- implement/initial/try.js
+++ implement/initial/try.js
@@ -1,14 +1,38 @@
+import paragraph from '../../data.json' with {type: 'json'};
+
 const para = document.getElementById("para")
 const icon = document.getElementById("img")
 const head = document.getElementById("celeb-H")
 const parag = document.getElementById("celeb-p")
+const rando = Math.floor(Math.random()*10)
 para.style.filter = "blur(2px)"
-const paragraph = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate. Doloribus, voluptate."
-const char = paragraph.split('')
+let text;
 
-char.forEach(element => {
-    const span = document.createElement('span')
-    span.innerText = element
-    span.classList.add('char')
-    para.appendChild(span)
-});
+const buttons = document.querySelectorAll('.bt')
+
+buttons.forEach(button => {
+    button.addEventListener('click',()=>{
+        const level = button.getAttribute('data-level');
+        setFunc(level)
+    })
+})
+
+function setFunc(n){
+    if (n === 'easy') {
+        text = paragraph.easy[rando].text
+    }
+    else if (n === 'medium') {
+        text = paragraph.medium[rando].text
+    }
+    else{
+        text = paragraph.hard[rando].text
+    }
+    const char = text.split('')
+
+    char.forEach(element => {
+        const span = document.createElement('span')
+        span.innerText = element
+        span.classList.add('char')
+        para.appendChild(span)
+    });
+}
